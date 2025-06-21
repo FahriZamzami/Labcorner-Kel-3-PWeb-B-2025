668529c9ae292cd76b0552b370f5824317fbc618
VaniaTania <vannesatania06@gmail.com>
2025-06-21 14:59:55 +0700

    commit

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middlewares/upload');
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-const { PrismaClient } = require('@prisma/client');
-=======
->>>>>>> 7d0a91d (update mahasiswa)
 const ExcelJS = require('exceljs');
 const PDFDocument = require('pdfkit');
 
@@ -19,13 +13,10 @@ const ekspor = require('../controllers/export.controller');
 // === Assignment Routes ===
 router.get('/assignments', assignmentsController.getAllAssignments);
 router.post('/assignments', upload.single('fileTugas'), assignmentsController.createAssignment);
->>>>>>> d58c348 (new update)
 
 // === Auth Routes ===
 router.post('/login', authenticationController.login);
 
-<<<<<<< HEAD
-=======
 // === Lab Routes ===
 router.get('/lab', labController.labPage);
 router.get('/lab/:id', labController.showHomeClassPage);
@@ -34,15 +25,11 @@ router.get('/lab/:id', labController.showHomeClassPage);
 router.get('/mahasiswa', mahasiswa.searchMahasiswa);
 router.get('/filter-semester', mahasiswa.getMahasiswaBySemester);
 router.post('/update-status/:id', mahasiswa.updateStatusMahasiswa);
-router.get('/mahasiswa/rekap', mahasiswa.getDaftarMahasiswa); 
+router.get('/mahasiswa/rekap', mahasiswa.getDaftarMahasiswa);
 
 // === Export Routes ===
 router.get('/export/pdf', ekspor.exportPDF);
 router.get('/export/excel', ekspor.exportExcel);
 
-<<<<<<< HEAD
->>>>>>> d58c348 (new update)
-=======
 // === Export router ONLY ===
->>>>>>> 7d0a91d (update mahasiswa)
 module.exports = router;
