cd5a56d3a1ebab71b22c088f0f2620e3f76b6add
Fahri Zamzami <fahrizamzami78242@gmail.com>
2025-06-22 20:19:37 +0700

    dibuat lebih rapi

--- src/index.js
+++ src/index.js
@@ -11,10 +11,12 @@ const {
     getPengumpulanByTugasId, getFilesByTugasId, beriNilaiForm, simpanNilai, createAssignmentForm
 } = require('./controllers/assignment.controller');
 
-const {
-    tampilkanRekapNilai,
-    exportRekapExcel,
-    exportRekapPDF
+const { 
+    tampilkanRekapNilai, 
+    exportRekapExcel, 
+    exportRekapPDF,
+    exportKehadiranExcel, // <-- Tambahkan
+    exportKehadiranPDF    // <-- Tambahkan
 } = require('./controllers/praktikum.controller');
 
 const {
@@ -34,45 +36,36 @@ const {
     createJadwal,
     getJadwalById,
     updateJadwal,
-    deleteJadwal
+    deleteJadwal,
+    toggleJadwalStatus,
+    getJadwalLabPage
 } = require('./controllers/jadwal.controller');
 
-// TAMBAHAN: Impor controller untuk Mahasiswa & Absensi
-const { getDaftarMahasiswaPage } = require('./controllers/mahasiswa.controller');
+const { getDaftarMahasiswaPage, exportDaftarMahasiswaExcel, exportDaftarMahasiswaPDF } = require('./controllers/mahasiswa.controller');
 const { getAbsensiPage, saveAbsensi, getDetailKehadiranPage } = require('./controllers/absensi.controller');
-const { 
-    getKuisPage, 
-    showTambahKuisPage, 
-    createKuis, 
-    showTambahSoalPage, 
-    createSoal,
-    deleteKuis,
-    toggleKuisStatus,
-    showEditKuisPage,
-    updateKuis,
-    showDaftarSoalPage,
-    showNilaiKuisPage,
-    exportNilaiExcel,
-    exportNilaiPDF
+
+const {
+    getKuisPage, showTambahKuisPage, createKuis, showTambahSoalPage, createSoal,
+    deleteKuis, toggleKuisStatus, showEditKuisPage, updateKuis,
+    showDaftarSoalPage, showNilaiKuisPage, exportNilaiExcel, exportNilaiPDF
 } = require('./controllers/kuis.controller');
 
 const { login } = require('./controllers/authentication.controller');
-const { labPage, showHomeClassPage } = require('./controllers/lab.controller');
+const { labPage, showHomeClassPage, getDaftarMahasiswaLabPage } = require('./controllers/lab.controller');
 const { getHomePage } = require('./controllers/home.controller');
-
-// TAMBAHAN: Impor controller untuk halaman Pilih Lab
+const { getPilihPraktikumPage, joinPraktikum } = require('./controllers/pilihlab.controller');
 const { 
-    getPilihPraktikumPage, 
-    joinPraktikum 
-} = require('./controllers/pilihlab.controller');
+    getPengumumanPage, createPengumuman, deletePengumuman 
+} = require('./controllers/pengumuman.controller');
+const { 
+    showTambahKelasPage, createKelas, showEditKelasPage, updateKelas, deleteKelas 
+} = require('./controllers/tambahKelas.controller');
 
-
-// ===== Import Middleware & Router Lain =====
+// ===== Import Middleware =====
 const { isAuthenticated } = require('./middlewares/auth');
 const upload = require('./middlewares/upload');
 const setCurrentPath = require('./middlewares/currentPage');
 const apiRouter = require('./routes/router');
-
 
 // ===== Middleware & Konfigurasi =====
 app.use(session({
@@ -98,13 +91,10 @@ app.get('/download/:filename', (req, res) => {
     }
 });
 
-// ===== Scheduler =====
 require('./scheduler');
 
 // ===== ROUTES =====
-app.get('/', (req, res) => {
-    res.redirect('/login');
-});
+app.get('/', (req, res) => res.redirect('/login'));
 
 // === Auth ===
 app.get('/login', (req, res) => res.render('login'));
@@ -113,7 +103,7 @@ app.get('/register', showRegisterPage);
 app.post('/register', handleFirstStep);
 app.post('/register/create', handleRegisterCreate);
 
-// 🔒 Semua route setelah ini butuh login
+// === Protected Routes ===
 app.use(isAuthenticated);
 app.use(setCurrentPath);
 
@@ -127,28 +117,23 @@ app.get('/assignments/:id/edit', editAssignmentForm);
 app.post('/penugasan/edit/:id', upload.single('fileTugas'), updateAssignment);
 app.post('/assignments/delete/:id', deleteAssignment);
 app.get('/penugasan/detail/:id', detailAssignment);
+app.get('/assignments/:id/pengumpulan', getPengumpulanByTugasId);
+app.get('/assignments/:id/files', getFilesByTugasId);
+app.get('/assignments/nilai/:id', beriNilaiForm);
+app.post('/assignments/nilai/:id', simpanNilai);
 
-// === Halaman Pilih Lab/Praktikum (untuk gabung kelas baru) === (TAMBAHAN BARU)
+// === Pilih Praktikum ===
 app.get('/pilihLab', getPilihPraktikumPage);
 app.post('/pilihLab/joinLab', joinPraktikum);
 
 // === Lab & Kelas ===
-const { 
-    showTambahKelasPage, 
-    createKelas,
-    showEditKelasPage,
-    updateKelas,
-    deleteKelas
-} = require('./controllers/tambahKelas.controller');
-
-// Urutan penting: Rute spesifik harus di atas rute dinamis
+app.get('/lab', labPage);
+app.get('/lab/:lab_id/mahasiswa', getDaftarMahasiswaLabPage);
 app.get('/kelas/tambah', showTambahKelasPage);
 app.post('/kelas/create', createKelas);
 app.get('/kelas/:id/edit', showEditKelasPage);
 app.post('/kelas/:id/update', updateKelas);
 app.post('/kelas/:id/delete', deleteKelas);
-
-app.get('/lab', labPage);
 app.get('/kelas/:id', showHomeClassPage);
 
 // === Modul ===
@@ -158,12 +143,7 @@ app.post('/modul/delete/:modul_id', deleteModul);
 
 // === Pengumuman ===
 app.get('/praktikum/:praktikum_id/pengumuman', getPengumumanPage);
-app.post('/pengumuman/create', createPengumuman);
-app.post('/pengumuman/delete/:id', deletePengumuman);
-
-// === Pengumuman (Informasi) ===
 app.post('/praktikum/:praktikum_id/pengumuman/create', createPengumuman);
-app.post('/pengumuman/update/:id', updatePengumuman);
 app.post('/pengumuman/delete/:id', deletePengumuman);
 
 // === Kuis ===
@@ -182,6 +162,7 @@ app.get('/kuis/:id/nilai/excel', exportNilaiExcel);
 app.get('/kuis/:id/nilai/pdf', exportNilaiPDF);
 
 // === Jadwal ===
+app.get('/lab/:lab_id/jadwal', getJadwalLabPage);
 app.get('/praktikum/:praktikum_id/jadwal', getJadwalPage);
 app.post('/jadwal/create', createJadwal);
 app.post('/jadwal/update/:id', updateJadwal);
@@ -191,117 +172,25 @@ app.get('/api/jadwal/:id', getJadwalById);
 
 // === Mahasiswa & Absensi ===
 app.get('/praktikum/:praktikum_id/mahasiswa', getDaftarMahasiswaPage);
+app.get('/praktikum/:praktikum_id/mahasiswa/excel', exportDaftarMahasiswaExcel);
+app.get('/praktikum/:praktikum_id/mahasiswa/pdf', exportDaftarMahasiswaPDF);
 app.get('/praktikum/:praktikum_id/absensi', getAbsensiPage);
 app.post('/absensi/save', saveAbsensi);
 app.get('/praktikum/:praktikum_id/absensi/detail', getDetailKehadiranPage);
-
-// === Assignment & Penilaian ===
-app.get('/assignments', getAllAssignments);
-app.get('/assignments/create', createAssignmentForm);
-app.get('/assignments/:id/edit', editAssignmentForm);
-app.post('/penugasan/edit/:id', upload.single('fileTugas'), updateAssignment);
-app.post('/assignments/delete/:id', deleteAssignment);
-app.get('/penugasan/detail/:id', detailAssignment);
-app.get('/assignments/:id/pengumpulan', getPengumpulanByTugasId);
-app.get('/assignments/:id/files', getFilesByTugasId);
-app.get('/assignments/nilai/:id', beriNilaiForm);
-app.post('/assignments/nilai/:id', simpanNilai);
 
 // === Rekap Nilai ===
 app.get('/praktikum/:id/rekap-nilai', tampilkanRekapNilai);
 app.get('/praktikum/:id/rekap-nilai/pdf', exportRekapPDF);
 app.get('/praktikum/:id/rekap-nilai/excel', exportRekapExcel);
-
-app.get('/pilihLab', getPilihPraktikumPage);
-
-// === Lab & Kelas ===
-const { 
-    showTambahKelasPage, 
-    createKelas,
-    showEditKelasPage,
-    updateKelas,
-    deleteKelas
-} = require('./controllers/tambahKelas.controller');
-
-// Urutan penting: Rute spesifik harus di atas rute dinamis
-app.get('/kelas/tambah', showTambahKelasPage);
-app.post('/kelas/create', createKelas);
-app.get('/kelas/:id/edit', showEditKelasPage);
-app.post('/kelas/:id/update', updateKelas);
-app.post('/kelas/:id/delete', deleteKelas);
-
-app.get('/lab', labPage);
-app.get('/kelas/:id', showHomeClassPage);
-
-// === Modul ===
-app.get('/praktikum/:praktikum_id/modul', getModulPage);
-app.post('/praktikum/:praktikum_id/modul/upload', upload.single('fileModul'), uploadModul);
-app.post('/modul/delete/:modul_id', deleteModul);
-
-// === Pengumuman ===
-app.get('/praktikum/:praktikum_id/pengumuman', getPengumumanPage);
-app.post('/pengumuman/create', createPengumuman);
-app.post('/pengumuman/delete/:id', deletePengumuman);
-
-// === Pengumuman (Informasi) ===
-app.post('/praktikum/:praktikum_id/pengumuman/create', createPengumuman);
-app.post('/pengumuman/update/:id', updatePengumuman);
-app.post('/pengumuman/delete/:id', deletePengumuman);
-
-// === Kuis ===
-app.get('/praktikum/:praktikum_id/kuis', getKuisPage);
-app.get('/praktikum/:praktikum_id/kuis/baru', showTambahKuisPage);
-app.post('/kuis/create', createKuis);
-app.get('/kuis/:id/soal/tambah', showTambahSoalPage);
-app.post('/kuis/:id/soal/tambah', createSoal);
-app.post('/kuis/:id/delete', deleteKuis);
-app.post('/kuis/:id/toggle-status', toggleKuisStatus);
-app.get('/kuis/:id/edit', showEditKuisPage);
-app.post('/kuis/:id/edit', updateKuis);
-app.get('/kuis/:id/daftar-soal', showDaftarSoalPage);
-app.get('/kuis/:id/nilai', showNilaiKuisPage);
-app.get('/kuis/:id/nilai/excel', exportNilaiExcel);
-app.get('/kuis/:id/nilai/pdf', exportNilaiPDF);
-
-// === Jadwal ===
-app.get('/praktikum/:praktikum_id/jadwal', getJadwalPage);
-app.post('/jadwal/create', createJadwal);
-app.post('/jadwal/update/:id', updateJadwal);
-app.post('/jadwal/delete/:id', deleteJadwal);
-app.post('/jadwal/toggle-status/:id', toggleJadwalStatus);
-app.get('/api/jadwal/:id', getJadwalById);
-
-// === Mahasiswa & Absensi ===
-app.get('/praktikum/:praktikum_id/mahasiswa', getDaftarMahasiswaPage);
-app.get('/praktikum/:praktikum_id/absensi', getAbsensiPage);
-app.post('/absensi/save', saveAbsensi);
-app.get('/praktikum/:praktikum_id/absensi/detail', getDetailKehadiranPage);
-
-// === Assignment & Penilaian ===
-app.get('/assignments', getAllAssignments);
-app.get('/assignments/create', createAssignmentForm);
-app.get('/assignments/:id/edit', editAssignmentForm);
-app.post('/penugasan/edit/:id', upload.single('fileTugas'), updateAssignment);
-app.post('/assignments/delete/:id', deleteAssignment);
-app.get('/penugasan/detail/:id', detailAssignment);
-app.get('/assignments/:id/pengumpulan', getPengumpulanByTugasId);
-app.get('/assignments/:id/files', getFilesByTugasId);
-app.get('/assignments/nilai/:id', beriNilaiForm);
-app.post('/assignments/nilai/:id', simpanNilai);
-
-// === Rekap Nilai ===
-app.get('/praktikum/:id/rekap-nilai', tampilkanRekapNilai);
-app.get('/praktikum/:id/rekap-nilai/pdf', exportRekapPDF);
-app.get('/praktikum/:id/rekap-nilai/excel', exportRekapExcel);
+app.get('/praktikum/:id/kehadiran/excel', exportKehadiranExcel);
+app.get('/praktikum/:id/kehadiran/pdf', exportKehadiranPDF);
 
 // === API ===
 app.use('/api', apiRouter);
 
 // === Logout ===
 app.get('/logout', (req, res) => {
-    req.session.destroy(() => {
-        res.redirect('/login');
-    });
+    req.session.destroy(() => res.redirect('/login'));
 });
 
 // ===== START SERVER =====
